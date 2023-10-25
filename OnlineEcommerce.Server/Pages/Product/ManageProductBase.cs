b27e679449ac3a7e18b0d8199c61ec81b2ad70ec
Jesriel <layugjesriel61@gmail.com>
2023-10-26 00:38:38 +0800

    Starting to create the manage product section.

--- OnlineEcommerce.Server/Pages/Product/ManageProductBase.cs
+++ OnlineEcommerce.Server/Pages/Product/ManageProductBase.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Components;
 using OnlineEcommerce.Server.Models.DTOs;
+using OnlineEcommerce.Server.Services.Contracts;
 
 namespace OnlineEcommerce.Server.Pages.Product
 {
     public class ManageProductBase : ComponentBase
     {
+        [Inject]
+        IProductService ProductService { get; set; }
+
+        public string searchString1 = "";
+
         public DTO_Product selectedProduct = null;
         public HashSet<DTO_Product> selectedProducts = new HashSet<DTO_Product>();
         public IEnumerable<DTO_Product> Products = new List<DTO_Product>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Products = await ProductService.GetProducts();
+        }
+
+        public bool FilterProduct1(DTO_Product product) => FilterProduct(product, searchString1);
+
+        public bool FilterProduct(DTO_Product product, string searchString)
+        {
+            var stock = product.Variants.FirstOrDefault(s => s.Stock.ToString() == searchString);
+            var sku = product.Variants.FirstOrDefault(s => s.SKU.ToString() == searchString);
+            var size = product.Variants.FirstOrDefault(s => s.Size.ToString() == searchString);
+            var color = product.Variants.FirstOrDefault(s => s.Color.ToString() == searchString);
+
+            if (string.IsNullOrWhiteSpace(searchString))
+                return true;
+            if (product.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+                return true;
+            if ($"{stock} {sku} {size} {color}".Contains(searchString))
+                return true;
+
+            return false;
+        }
     }
 }
